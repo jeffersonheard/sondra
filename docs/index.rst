ffb7dac10bdf17461ab05288c3bc10bb5b1fbc67
Jefferson Heard <jeff@terrahub.io>
2015-10-06 10:48:18 -0400

    added docs

---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. sondra documentation master file, created by
+   sphinx-quickstart on Tue Oct  6 10:46:40 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to sondra's documentation!
+==================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='sondra',
+    packages='sondra',
+    version='0.0.1',
+    description='JSON-Schema-based ORM for RethinkDB',
+    author="Jefferson Heard",
+    author_email="jefferson.r.heard@gmail.com",
+    url="https://github.com/JeffHeard/sondra",
+    keywords=["json","rethinkdb","flask"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Flask",
+        "Framework :: Django",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Database",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries",
+        "Natural Language :: English",
+   ]
+)
