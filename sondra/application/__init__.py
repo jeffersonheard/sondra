ac4dd832ad3185d9771c3cebde28de03843b4628
Jefferson Heard <jeff@terrahub.io>
2015-10-12 21:36:26 -0400

    Added docstrings.

--- sondra/application/__init__.py
+++ sondra/application/__init__.py
@@ -53,10 +53,42 @@ class ApplicationMetaclass(ABCMeta):
 
 
 class Application(Mapping, metaclass=ApplicationMetaclass):
-    """A reusable group of :class:`Collections` and optional top-level exposed functionality.
+    """An Application groups collections that serve a related purpose.
 
-    An Application can contain any number of :class:`Collection`s.
+    In addition to collections, methods and schemas can be exposed at the application level. Any exposed methods
+    might be termed "library functions" in the sense that they apply to all collections, or configure the collections
+    at a high level. Schemas exposed on the application level should be common to several collections or somehow
+    logically "broader" than definitions at the document/collection level.
 
+    Application behave as Python dicts. The keys in the application's dictionary are the slugs of the collections the
+    application houses. In addition, applications are stored as items in a Suite's dictionary. Thus to access the
+    'Users' collection in the 'Auth' application, one could start with the suite and work down thus::
+
+        > suite['auth']['users']
+        ...
+        <Application object 0x...>
+
+    Also see the `webservices reference`_ for more on how to access applications and their schemas and
+    methods over the web.
+
+    Attributes:
+        db (str): The name of a RethinkDB database
+        connection (str): The name of a RethinkDB connection in the application's suite
+        slug (str): read-only. The name of this application class, slugified (all lowercase, and separate words with -)
+        anonymous_reads (bool=True): Override this attribute in your subclass if you want to disable anonymous queries
+          for help and schema for this application and all its collections.
+        definitions (dict): This should be a JSON serializable dictionary of schemas. Each key will be the name of that
+          schema definiton in this application schema's "definitions" object.
+        url (str): read-only. The full URL for this application.
+        schema_url (str): read-only. A shortcut to the application's schema URL.
+        schema (dict): read-only. A JSON serializable schema definition for this application. In addition to the
+          standard JSON-schema definitions, a dictionary of collection schema URLs and a list of methods are included as
+          "collections" and "methods" respectively.  The keys for the collection schemas are the slugged names of the
+          collections themselves.
+        full_schema (dict): Same as schema, except that collections are fully defined instead of merely referenced in
+          the "collections" sub-object.
+
+    ..webservices reference: /docs/web-services.html
     """
     db = 'default'
     connection = 'default'
@@ -101,7 +133,16 @@ class Application(Mapping, metaclass=ApplicationMetaclass):
         }
 
     def help(self, out=None, initial_heading_level=0):
-        """Return full reStructuredText help for this class"""
+        """Return full reStructuredText help for this class.
+
+        Args:
+            out (io): An output, usually io.StringIO
+            initial_heading_level (int): 0-5, default 0. The heading level to start at, in case this is being included
+              as part of a broader help scheme.
+
+        Returns:
+            (str): reStructuredText help.
+        """
         builder = help.SchemaHelpBuilder(self.schema, self.url, out=out, initial_heading_level=initial_heading_level)
         builder.begin_subheading(self.name)
         builder.begin_list()
@@ -131,6 +172,13 @@ class Application(Mapping, metaclass=ApplicationMetaclass):
 
 
     def __init__(self, suite, name=None):
+        """Create a new instance of the application.
+
+        Args:
+            suite (sondra.suite.Suite): The suite with which to register the application.
+            name (str): If supplied, this is the name of the app.  Otherwise, the name is the same as the classname
+              The application slug is the slugified version of the name.
+        """
         self.suite = suite
         self.name = name or self.__class__.__name__
         self.slug = utils.camelcase_slugify(self.name)
@@ -163,6 +211,25 @@ class Application(Mapping, metaclass=ApplicationMetaclass):
         return item in self.collections
 
     def create_tables(self, *args, **kwargs):
+        """Create tables in the db for all collections in the application.
+
+        If the table exists, log a warning.
+
+        **Signals sent:**
+
+        pre_create_tables(instance=``self``, args=``args``, kwargs=``kwargs``)
+          Sent before tables are created
+
+        post_create_tables(instance=``self``)
+          Sent after the tables are created
+
+        Args:
+            *args: Sent to collection.create_table as vargs.
+            **kwargs: Sent to collection.create_table as keyword args.
+
+        Returns:
+            None
+        """
         signals.pre_create_tables.send(self.__class__, instance=self, args=args, kwargs=kwargs)
 
         for collection_class in self.collections.values():
@@ -174,6 +241,26 @@ class Application(Mapping, metaclass=ApplicationMetaclass):
         signals.post_create_tables.send(self.__class__, instance=self)
 
     def drop_tables(self, *args, **kwargs):
+        """Create tables in the db for all collections in the application.
+
+        If the table exists, log a warning.
+
+        **Signals sent:**
+
+        pre_delete_tables(instance=``self``, args=``args``, kwargs=``kwargs``)
+          Sent before tables are created
+
+        post_delete_tables(instance=``self``)
+          Sent after the tables are created
+
+        Args:
+            *args: Sent to collection.delete_table as vargs.
+            **kwargs: Sent to collection.delete_table as keyword args.
+
+        Returns:
+            None
+        """
+
         signals.pre_delete_tables.send(self.__class__, instance=self, args=args, kwargs=kwargs)
 
         for collection_class in self.collections.values():
@@ -185,12 +272,28 @@ class Application(Mapping, metaclass=ApplicationMetaclass):
         signals.post_delete_tables.send(self.__class__, instance=self)
 
     def create_database(self):
+        """Create the db for the application.
+
+        If the db exists, log a warning.
+
+        Returns:
+            None
+        """
+
         try:
             r.db_create(self.db).run(self.connection)
         except r.ReqlError as e:
             self.log.warning(str(e))
 
     def drop_database(self):
+        """Drop the db for the application.
+
+        If the db exists, log a warning.
+
+        Returns:
+            None
+        """
+
         try:
             r.db_create(self.db).run(self.connection)
         except r.ReqlError as e:
