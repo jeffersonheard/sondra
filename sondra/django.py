461ed9780d742a7c292914c1b4be6fd2480ee0d1
Jefferson Heard <jeff@terrahub.io>
2015-10-13 12:38:59 -0400

    Made sure sondra was importable during the doc build.  Removed empty django.py

--- sondra/django.py
+++
@@ -1,8 +0,0 @@
-from .document import Suite
-from .api import APIRequest
-
-def create_api_request(request):
-    suite = document.Suite()
-    return APIRequest(suite, request.body, request.META, request.method, request.user,
-               request.path, request.REQUEST, request.FILES)
-
